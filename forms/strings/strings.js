be536f031c8e6fe136e5941691cb5ea609cbbf4f
Makala Smith <mms96354@creighton.edu>
2021-03-03 14:45:37 -0600

    complete string and found

--- forms/strings/strings.js
+++ forms/strings/strings.js
@@ -3,3 +3,13 @@ let quoteString = "Failure is simply the opportunity to begin again this time mo
 console.log(`Upper case string is: ${quoteString.toUpperCase()}`)
 
 let authorString = "- Henry Ford"
+
+let completeString = quoteString.concat(authorString)
+
+console.log(`Concatenated string is: ${completeString}`)
+
+let found = completeString.includes("Henry")
+if (found)
+  console.log("Henry was in completeString.")
+else 
+  console.log("Henry was NOT in completeString.")
